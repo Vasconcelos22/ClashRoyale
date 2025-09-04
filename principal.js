6470c62584403307a3d31ab81e42d06a3a9c8d4b
Vasconcelos22 <marcelo.vasconcelos.junior@escola.pr.gov.br>
2025-09-04 08:59:50 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,27 +1,47 @@
-var paciente = document.querySelector("h2");
-			paciente.textContent = "Meus Pacientes";
-			var paciente = document.querySelector("h1");
-			paciente.textContent = "Nutri Clash​";
-            
-            // acessar a tag tr - paciente Paulo
-            var paciente = document.querySelector("#primeiro-paciente");
-            var tdPeso = paciente.querySelector(".info-peso");
-            var peso = tdPeso.textContent;
-            
-            // acessar altura
-            var tdAltura = paciente.querySelector(".info-altura");
-            var altura = tdAltura.textContent;
-            
-            // calcula o imc
-            var imc = peso / (altura * altura);
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "Meus Pacientes";
+var Marcelo = document.querySelector("h1");
+Marcelo.textContent = "Nutri Clash​";
 
-            //acessa e altera o imc
-            var tdImc = paciente.querySelector(".info-imc");
-            tdImc.textContent = imc;
+// acessar a tag tr - paciente Paulo
+var paciente = document.querySelectorAll(".paciente");
 
-            if(peso < 0 || peso > 1000){
-                alert("Peso invalido");
-            }
-            if(altura < 0 || altura > 3.00){
-                alert("Altura invalida");
-            }
+for(var i = 0; i < paciente.length; i++){
+    //selecionar o peso da tag
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    // acessar altura
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    // calcula o imc
+    var imc = peso / (altura * altura);
+
+    //acessa e altera o imc
+    //var tdImc = paciente.querySelector(".info-imc");
+    //tdImc.textContent = imc;
+
+    //variáveis com valor true
+    var pesoValido = true;
+    var alturaValda = true; 
+
+    if(pesoValido && alturaValda){
+    var tdImc = paciente.querySelector(".info-imc");
+    tdImc.textContent = imc;
+    }
+
+    //define limites de peso e altura 
+    if(peso <=  0 || peso >= 1000){
+    alert("Peso invalido");
+    var pesoValido = false;
+    tdImc.textContent = "Peso Inválido";
+    }   
+
+    if(altura <= 0 || altura >= 3.00){
+    alert("Altura invalida");
+    var alturaValda = false;
+    tdImc.textContent = "Altura Inválido";
+    }
+}
+
